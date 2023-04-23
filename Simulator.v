b0a7c7f27d01ec3b8014ca02ef25894d5bd23da4
kilka-rav <zakirianov.rv@phystech.edu>
2023-04-24 00:22:28 +0300

    fix bugs

--- Simulator.v
+++ Simulator.v
@@ -43,15 +43,15 @@ wire [31:0] imm32_EX, rs1_val_EX;
 wire [1:0] NextPC_EX;
 wire [1:0] TakenNextPC_EX = (BranchIsTaken_EX) ? NextPC_EX : 2'b00;
 wire PCEn = !Stall_IF & !Exception_WB;
-RegPC pc_module(clk, PCEn, TakenNextPC_EX, pc_EX, imm32_EX, rs1_val_EX, pc_IF);
+PCCounter pc_module(clk, PCEn, TakenNextPC_EX, pc_EX, imm32_EX, rs1_val_EX, pc_IF);
 
 wire [31:0] instr_IF;
-MEM #(.N(17), .DW(32)) imem(clk, pc_IF >> 2, 3'b010 /*32w*/, 0/*we*/, 0, instr_IF);
+Memory #(.N(17), .DW(32)) imem(clk, pc_IF >> 2, 3'b010 /*32w*/, 0/*we*/, 0, instr_IF);
 
 wire [31:0] pc_ID, instr_ID;
 wire Enable_ID = !Stall_ID & !Exception_WB;
 wire rst_ID = rst | BranchIsTaken_EX;
-RegPipe #(.W(64)) fence_ID(clk, rst_ID, Enable_ID, 
+Pipeline #(.W(64)) fence_ID(clk, rst_ID, Enable_ID, 
 {pc_IF, instr_IF}, 
 {pc_ID, instr_ID}
 );
@@ -67,13 +67,13 @@ wire [3:0] alu_op_ID;
 wire [2:0] mem_width_ID;
 wire MemToReg_ID, MemWrite_ID, ALUSrc1_ID, RegWrite_ID, Branch_ID, InvertBranchTriger_ID, Jump_ID, Exception_ID, valid_ID;
 wire[1:0] ALUSrc2_ID, NextPC_ID;
-DECODER decoder(instr_ID, 
+Decoder decoder(instr_ID, 
     rs1n_ID, rs2n_ID, rdn_ID, imm32_ID, // decoded instruction
     mem_width_ID ,alu_op_ID, 
     MemToReg_ID, MemWrite_ID, ALUSrc1_ID, ALUSrc2_ID, RegWrite_ID, Branch_ID, InvertBranchTriger_ID, Jump_ID, NextPC_ID,
     Exception_ID, valid_ID
 );
-RGF reg_file(clk, rs1n_ID, rs2n_ID, RegWrite_WB, rdn_WB, Result_WB, //input
+RegisterFile reg_file(clk, rs1n_ID, rs2n_ID, RegWrite_WB, rdn_WB, Result_WB, //input
     rs1_val_ID, rs2_val_ID // out
 );
 // pipe register
@@ -86,11 +86,11 @@ wire [1:0] ALUSrc2_EX;
 
 wire PipeRegRst_EX = rst | Flush_EX | BranchIsTaken_EX;
 wire PipeRegEn_EX = !Exception_WB;
-RegPipe #(.W(150)) fence_ex_vals(clk, PipeRegRst_EX, PipeRegEn_EX, 
+Pipeline #(.W(150)) fence_ex_vals(clk, PipeRegRst_EX, PipeRegEn_EX, 
     {pc_ID, rs1_val_ID, rs2_val_ID, imm32_ID, rs1n_ID, rs2n_ID, rdn_ID, alu_op_ID, mem_width_ID}, 
     {pc_EX, rs1_val_EX, rs2_val_EX, imm32_EX, rs1n_EX, rs2n_EX, rdn_EX, alu_op_EX, mem_width_EX}
 );
-RegPipe #(.W(13)) fence_ex_flags(clk, PipeRegRst_EX, PipeRegEn_EX,
+Pipeline #(.W(13)) fence_ex_flags(clk, PipeRegRst_EX, PipeRegEn_EX,
     {MemToReg_ID, MemWrite_ID, ALUSrc1_ID, ALUSrc2_ID, RegWrite_ID, Branch_ID, InvertBranchTriger_ID, Jump_ID, NextPC_ID, Exception_ID, valid_ID},
     {MemToReg_EX, MemWrite_EX, ALUSrc1_EX, ALUSrc2_EX, RegWrite_EX, Branch_EX, InvertBranchTriger_EX, Jump_EX, NextPC_EX, Exception_EX, valid_EX}
 );
@@ -115,17 +115,17 @@ wire [2:0] mem_width_MEM;
 wire MemToReg_MEM, MemWrite_MEM, Exception_MEM, valid_MEM;
 wire PipeRegRst_MEM = rst;
 wire PipeRegEn_MEM = !Exception_WB;
-RegPipe #(.W(96)) fence_mem_values(clk, PipeRegRst_MEM, PipeRegEn_MEM, 
+Pipeline #(.W(96)) fence_mem_values(clk, PipeRegRst_MEM, PipeRegEn_MEM, 
 {pc_EX , ALUOut_EX , rs2_val_forwarded_EX}, 
 {pc_MEM, ALUOut_MEM, MemWriteData_MEM    }
 );
-RegPipe #(.W(13)) fence_mem_flags(clk, PipeRegRst_MEM, PipeRegEn_MEM,
+Pipeline #(.W(13)) fence_mem_flags(clk, PipeRegRst_MEM, PipeRegEn_MEM,
 {mem_width_EX , MemToReg_EX , MemWrite_EX , RegWrite_EX , rdn_EX , Exception_EX , valid_EX },
 {mem_width_MEM, MemToReg_MEM, MemWrite_MEM, RegWrite_MEM, rdn_MEM, Exception_MEM, valid_MEM}
 );
 wire[31:0] imm32_MEM;
 wire [4:0] rs1n_MEM, rs2n_MEM;
-RegPipe #(.W(42)) debug_pipe_MEM(clk, PipeRegRst_MEM, PipeRegEn_MEM,
+Pipeline #(.W(42)) debug_pipe_MEM(clk, PipeRegRst_MEM, PipeRegEn_MEM,
 {imm32_EX , rs1n_EX , rs2n_EX },
 {imm32_MEM, rs1n_MEM, rs2n_MEM}
 );
@@ -140,16 +140,16 @@ wire[31:0] ReadData_WB, ALUOut_WB;
 wire MemToReg_WB, valid_WB;
 wire PipeRegRst_WB = rst;
 wire PipeRegEn_WB = !Exception_WB;
-RegPipe #(.W(64)) fence_wb_vals(clk, PipeRegRst_WB, PipeRegEn_WB, 
+Pipeline #(.W(64)) fence_wb_vals(clk, PipeRegRst_WB, PipeRegEn_WB, 
 {ReadData_MEM, ALUOut_MEM}, 
 {ReadData_WB , ALUOut_WB });
-RegPipe #(.W(9)) fence_wb_flags(clk, PipeRegRst_WB, PipeRegEn_WB, 
+Pipeline #(.W(9)) fence_wb_flags(clk, PipeRegRst_WB, PipeRegEn_WB, 
 {RegWrite_MEM, MemToReg_MEM, rdn_MEM, Exception_MEM, valid_MEM}, 
 {RegWrite_WB , MemToReg_WB , rdn_WB , Exception_WB , valid_WB });
 // debug info pipe
 wire[31:0] imm32_WB, pc_WB;
 wire [4:0] rs1n_WB, rs2n_WB;
-RegPipe #(.W(74)) debug_pipe_WB(clk, PipeRegRst_WB, PipeRegEn_WB,
+Pipeline #(.W(74)) debug_pipe_WB(clk, PipeRegRst_WB, PipeRegEn_WB,
 {pc_MEM, imm32_MEM, rs1n_MEM, rs2n_MEM},
 {pc_WB , imm32_WB , rs1n_WB , rs2n_WB}
 );
